1ec764471d67f23d821d21e59aa0de52ff12fd76
Kingdom 1313 <admin@kd1313.com>
2026-06-16 23:33:55 +0800

    Remove unwanted KD1313 public text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer>
       <div>KD1313 | Kingdom of Driftwood</div>
-      <div>Unofficial community website. Rise of Kingdoms belongs to its respective owner.</div>
+      <div>Rise with the tide.</div>
     </footer>
   );
 }
